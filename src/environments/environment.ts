ec620545d19b7446803c569cefe453024617f68a
vandogouveia <vando_gouveia@hotmail.com>
2025-11-29 17:05:18 -0300

    remove logs

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,5 +3,6 @@ export const environment = {
   API_URL: 'https://cpsmcrateus.ce.gov.br/api-realstate',
   //API_URL: 'http://localhost:3001',
   API_IBGE: 'https://servicodados.ibge.gov.br/api/v1/localidades/estados',
+  
 };
 
